b35e4974bf7d1db20d4360c94aab7de5a97e3cc6
jp-walker <lwalker2677@scrippscollege.edu>
2023-05-05 17:01:00 -0700

    triggering tests

--- wardial.py
+++ wardial.py
@@ -227,11 +227,11 @@ def wardial(hosts, **kwargs):
     # Ensure that all of the kwargs parameters get passed to `_wardial_async`.
     # You will have to do some post-processing of the results of this function to convert the output.
     loop = asyncio.new_event_loop()
-    rets = loop.run_until_complete(_wardial_async(hosts))
+    booleans = loop.run_until_complete(_wardial_async(hosts))
     loop.close()
     ret = []
-    for i in range(len(rets)):
-        if rets[i]:
+    for i in range(len(booleans)):
+        if booleans[i]:
             ret.append(hosts[i])
     return ret
 
